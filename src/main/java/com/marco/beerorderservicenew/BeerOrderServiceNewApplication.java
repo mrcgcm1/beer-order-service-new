ec0cc8003e90ab6737e2960b37b39b47f4565e0b
Marco Giacomello <m.giacomello@insiel.it>
2022-12-29 16:10:46 +0100

    commit openfeing #8

--- src/main/java/com/marco/beerorderservicenew/BeerOrderServiceNewApplication.java
+++ src/main/java/com/marco/beerorderservicenew/BeerOrderServiceNewApplication.java
@@ -3,7 +3,9 @@ package com.marco.beerorderservicenew;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jms.artemis.ArtemisAutoConfiguration;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableFeignClients
 @SpringBootApplication(exclude = ArtemisAutoConfiguration.class)
 public class BeerOrderServiceNewApplication {
 
